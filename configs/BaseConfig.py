6389888e83231883c1a65c36c802e07ed156fc9b
Lmy0217 <lmy0217@126.com>
2019-09-17 20:37:28 +0800

    [update] BaseConfig.__repr__

--- configs/BaseConfig.py
+++ configs/BaseConfig.py
@@ -27,39 +27,31 @@ class BaseConfig(object):
             return False
 
     def __repr__(self):
-        s = list()
-        sp = list()
-        for i in range(self._space):
-            sp.append(' ')
+        s, sp = '', ' ' * self._space
         if self._space == 0:
-            s.extend(sp)
-            s.append(self.__class__.__name__)
+            s += self.__class__.__name__
             if hasattr(self, 'name'):
-                s.append(' (' + str(self.name) + ')')
-            s.append(': ')
-        s.append('{\n')
-        sp2 = sp.copy()
-        sp2.append('  ')
+                s += ' (' + str(self.name) + ')'
+            s += ': '
+        s += '{\n'
         v = list(vars(self).items())
         v.sort()
         for name, value in v:
             if not self._values(name):
                 continue
-            s.extend(sp2)
-            s.append(name + ': ')
+            s += sp + '  ' + name + ': '
             if value.__class__.__base__ == BaseConfig or value.__class__ == BaseConfig:
                 value._space = self._space + 2
             if isinstance(value, str):
-                s.append("'")
-            s.append(str(value))
+                s += "'"
+            s += str(value)
             if isinstance(value, str):
-                s.append("'")
-            s.append('\n')
-        s.extend(sp)
-        s.append('}')
+                s += "'"
+            s += '\n'
+        s += sp + '}'
         if self._space == 0:
-            s.append('\n')
-        return ''.join(s)
+            s += '\n'
+        return s
 
     def _load(self, config_dict):
         if isinstance(config_dict, dict):
